c1864be3d7d55f6deebce9b719ed3cf629eeb662
Tsai <liangzhetsai@outlook.com>
2025-06-19 16:52:39 +0800

    Complete name check

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -2,7 +2,11 @@ def main():
     email_to_name = {}
     email = input("Email: ")
     while email != "":
-        email_to_name[email] = extract_name(email)
+        name = extract_name(email)
+        confirm = input(f"Is your name {name}? (Y/n) ").strip().lower()
+        if confirm == "n":
+            name = input("Name: ")
+        email_to_name[email] = name
         email = input("Email: ")
     for email, name in email_to_name.items():
         print(f"{name} ({email})")
