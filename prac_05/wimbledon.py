bed48ed0ba8e7f95398c22f360187235ecef627e
Tsai <liangzhetsai@outlook.com>
2025-06-19 19:22:25 +0800

    Complete the count_champions function

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -8,6 +8,7 @@ def main():
     data = read_data(filename)
     print(data)
     champion_to_wins = count_champions(data)
+    print(champion_to_wins)
     countries = get_champion_countries(data)
     display_results(champion_to_wins, countries)
 
@@ -19,8 +20,13 @@ def read_data(filename):
             data_parts = line.strip().split(",")
             data.append(data_parts)
         return data
-def count_champions():
-    return
+
+def count_champions(data):
+    champion_to_wins = {}
+    for row in data:
+        champion = row[2]
+        champion_to_wins[champion] = champion_to_wins.get(champion, 0) + 1
+    return champion_to_wins
 
 def get_champion_countries():
     return
