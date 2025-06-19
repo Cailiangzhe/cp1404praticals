1c362832ce280022eacc8cdb4d9af26037c5d483
Tsai <liangzhetsai@outlook.com>
2025-06-19 16:08:03 +0800

    Sorted as instructions

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -9,3 +9,6 @@ word_dict = {}
 for word in words:
     word = word.lower()
     word_dict[word] = word_dict.get(word,0) + 1
+max_len = max(len(word) for word in word_dict)
+for word in sorted(word_dict):
+    print(f"{word:{max_len}}:{word_dict[word]}")
