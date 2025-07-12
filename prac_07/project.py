81e54e7c6e3903fb51fcb4d79fdb41a388ff5efd
Tsai <liangzhetsai@outlook.com>
2025-07-12 21:51:05 +0800

    initialize the project object

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,2 +1,11 @@
+import datetime
+
+
 class Project:
-    pass
+    def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
+        self.name = name
+        self.start_date = datetime.datetime.strptime(start_date, "%d/%m/%Y").date()
+        self.priority = int(priority)
+        self.cost_estimate = float(cost_estimate)
+        self.completion_percentage = int(completion_percentage)
+
