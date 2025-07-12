8970c6c2e0132da8cfd70af2dc99c533664db49c
Tsai <liangzhetsai@outlook.com>
2025-07-12 18:17:43 +0800

    Complete the main function menu display and user interaction functions.

---
+++ prac_07/project_managment.py
@@ -0,0 +1,45 @@
+"""
+Project Management Program
+Estimated time: 2.5 hours
+Actual time:  hours
+"""
+
+from project import Project
+import datetime
+
+FILENAME = "projects.txt"
+
+
+def main():
+    print("Welcome to Pythonic Project Management")
+    projects = load_projects(FILENAME)
+    print(f"Loaded {len(projects)} projects from {FILENAME}")
+    menu = "\n- (L)oad projects\n- (S)ave projects\n- (D)isplay projects\n- (F)ilter projects by date\n- (A)dd new project\n- (U)pdate project\n- (Q)uit"
+    choice = ""
+    while choice.lower() != "q":
+        print(menu)
+        choice = input(">>> ").lower()
+        if choice == "l":
+            filename = input("Filename: ")
+            projects = load_projects(filename)
+        elif choice == "s":
+            filename = input("Filename: ")
+            pass
+        elif choice == "d":
+            pass
+        elif choice == "f":
+            date_str = input("Show projects that start after date (dd/mm/yyyy): ")
+            pass
+        elif choice == "a":
+            pass
+        elif choice == "u":
+            pass
+        elif choice == "q":
+            confirm = input(f"Would you like to save to {FILENAME}? ").lower()
+            if confirm =="y" or confirm =="yes":
+                pass
+    print("Thank you for using custom-built project management software.")
+
+
+def load_projects(filename):
+    pass
