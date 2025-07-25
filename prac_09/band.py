6c9094b0493b14abcdc15e498a9af9fb1cc07898
Tsai <liangzhetsai@outlook.com>
2025-07-25 12:12:32 +0800

    Complete __init__ function

---
+++ prac_09/band.py
@@ -0,0 +1,8 @@
+class Band:
+    """Band class - A band has a name and a list of musicians."""
+
+    def __init__(self, name):
+        """Initialise Band with name and empty list of musicians."""
+        self.name = name
+        self.musicians = []
+
